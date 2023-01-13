c101cad852f3154c56ddc639016e1ea415a0ba29
sreethebee <122555167+sreethebee@users.noreply.github.com>
2023-01-13 12:25:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        # . . . #
-        # . . . .
-        # # # . #
-        # . # . #
-        # . # . #
-        `)
+    basic.showString("have a great day!")
+    basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.ForeverInBackground)
+    basic.showString("youre awesome")
     basic.showIcon(IconNames.SmallHeart)
 })
-basic.showIcon(IconNames.Happy)
+basic.showString("hi!")
+basic.forever(function () {
+    music.playMelody("C A D B G C5 F E ", 120)
+})
